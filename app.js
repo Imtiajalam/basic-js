e924281b6a73815949e33f8a9b776048003f9f75
imtiaj alam <imtiajalam72@gmail.com>
2020-07-24 03:25:11 +0600

    addind code

---
+++ app.js
@@ -0,0 +1,209 @@
+// var----------------
+
+// var name = "Imtiaj ALam";
+
+// name = "rafee";                                                //reassignable
+// console.log(name);
+
+// var age = 12;
+// console.log(age);
+
+
+// let---------------
+
+// let name = "maria";
+// console.log(name);
+
+// const ---------
+
+// const name = "Alam"                                         //not reassignable .It should be uniq 
+// console.log(name);
+
+
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+// single math with Number------------
+// val = num1+num2;
+// val = num1-num2;
+// val = num1*num2;
+// val = num1/num2;
+// val = num1%num2;
+
+// math Object------------------------
+// val =Math.PI;
+// val = Math.round(2.6);
+// val = Math.sqrt(63);
+// val = math.randon();
+// console.log(val);
+
+// String------------------------
+// const  firstName = "siyam";
+// const secondName = "alam";
+
+// let val ;
+
+// val = firstName +" "+ secondName;                  //("") for space(its called concatenation )
+// console.log(val);
+
+// // dynamic ---------------------------
+// const name ="siyam";
+// const age =21;
+// const job = "web Developer";
+// const city = "dhaka";
+
+// // templete String dynamic------------------------
+
+// let html ;
+// html = `
+// <ul>
+//     <li> Name : ${name}</li>
+//     <li> Name : ${age}</li>
+//     <li> Name : ${job}</li>
+//     <li> Name : ${city}</li>
+
+// </ul>
+// `
+// document.body.innerHTML = html;
+
+
+
+// if else--------------------
+
+// const id = 100;
+// Equal to---------------
+// if (id == 100){
+//     console.log("correct");
+// }
+// else{
+//     console.log("incorrect");
+// }
+//not  Equal to---------------
+
+// if (id != 100){
+//     console.log("correct");
+// }
+// else{
+//     console.log("incorrect");
+// }
+
+// equal  to value and type--------------------
+
+
+// if (id === 100){
+//     console.log("correct");
+// }
+// else{
+//     console.log("incorrect");
+// }
+
+
+// not equal to---------------------
+
+
+// if (id !== 100){
+//     console.log("correct");
+// }
+// else{
+//     console.log("incorrect");
+// }
+
+// test of undefined--------------------
+
+// if(typeof id !== undefined){
+//     console.log(`THE ID is $(id) `);
+// }
+// else{
+//     console.log("no ID");
+// }
+
+
+
+// else if---------------------
+
+// var name = "blue";
+
+// if (name === "red"){
+//     console.log("the color is red");
+// }
+// else if (name === "blue"){
+//     console.log("the color is blue");
+// }
+// else{
+//     console.log("the color is not blue or red");
+// }
+
+// // greater or less---------------------------------------
+// if (id > 1100){
+//     console.log("correct");
+// }
+// else{
+//     console.log("incorrect");
+// }
+
+
+// logical operator
+
+// const name = "siyam"
+// const age = 24;
+
+// and &&----------------------
+
+// if (age > 0 && age <12 ){
+//     console.log(`${name}is a child`);
+// }
+// else if (age >= 13 && age <19){
+//     console.log(`${name}is a teenager`);
+// }
+// else{
+//     console.log(`${name} is an adult`);
+// }
+
+// or---------------------
+
+// switch case
+
+// const color ="red";
+
+// switch (color) {
+//     case "red":
+//         console.log("The color is red");
+//         break;
+//     case "blue":
+//         console.log("The color is blue");
+//         break;
+
+//     default:
+//         console.log("the color is not red or blue");
+// }
+
+// for cheaking day of week---------------------
+
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = "sunday"
+//         break;
+//     case 1:
+//         day ="monday"
+//         break;
+//     case 2:
+//         day = "tuesday"
+//         break;
+//     case 3:
+//         day = "wednesday"
+//         break;
+//     case 4:
+//         day = "thursday"
+//         break;
+//     case 5:
+//         day = "friday"
+//         break;
+//     case 6:
+//         day = "saturday"
+//         break;
+// }
+//     console.log(`today is ${day}`);
+
+
+// array-----------------------------
